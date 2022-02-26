4d8d9282e3b68b0ade49da8dc42a721d2304dc09
jj-iwannajob <79934688+jj-iwannajob@users.noreply.github.com>
2022-02-26 10:43:07 +0900

    Create Variable01.java

---
+++ Chapter01_Variable/Variable01.java
@@ -0,0 +1,10 @@
+package 패키지명
+
+public class Variable01{
+  public static void main(String[] args) {
+    //변수(Variable) 정리
+    //변수란 무엇일까? 중고등학교 수학에서는 '변하는 수'라고 해서 배웠는데, 자바에서는 어떤 것이 변수일까?
+    
+
+  }
+}
